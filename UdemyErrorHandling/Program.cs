1592058ed9607dc37aa7e23d8e3df04ee041c58a
Emre Yılmaz <yilmazemrecu@gmail.com>
2024-05-19 13:23:42 +0300

    Add project files.

---
+++ UdemyErrorHandling/Program.cs
@@ -0,0 +1,29 @@
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+else
+{
+
+    app.UseHsts();
+}
+app.UseExceptionHandler("/Home/Error");
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
