6a0768851e92e0b6b9dc31c37b5f587899b61477
Emre Yılmaz <yilmazemrecu@gmail.com>
2024-05-19 13:43:17 +0300

    ErrorHandling Middleware Example

--- UdemyErrorHandling/Controllers/HomeController.cs
+++ UdemyErrorHandling/Controllers/HomeController.cs
@@ -1,35 +1,43 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 using UdemyErrorHandling.Models;
 
 namespace UdemyErrorHandling.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+	public class HomeController : Controller
+	{
+		private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+		public HomeController(ILogger<HomeController> logger)
+		{
+			_logger = logger;
+		}
 
-        public IActionResult Index()
-        {
-            int value1 = 5;
-            int value2 = 0;
-            int result = value1 / value2;
-            return View();
-        }
+		public IActionResult Index()
+		{
+			int value1 = 5;
+			int value2 = 0;
+			int result = value1 / value2;
+			return View();
+		}
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+		public IActionResult Privacy()
+		{
+			throw new FileNotFoundException();
+			return View();
+		}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		[AllowAnonymous]
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			//ExceptionHandler Path Feature ile httpcontext yardýmýyla hata yakalanýr ardýndan viewbag ile error viewine gönderilir .
+			var exception = HttpContext.Features.Get<IExceptionHandlerPathFeature>();
+			ViewBag.path = exception?.Path;
+			ViewBag.message = exception?.Error.Message;
+			return View();
+		}
+	}
 }
